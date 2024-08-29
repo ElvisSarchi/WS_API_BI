fa9438797945a210f662737de6a4cc1f2aabad3c
ElvisSarchi <sacianex2@gmail.com>
2024-08-29 14:43:11 -0500

    feat: Add new endpoints for consulta de tabla and BI integration

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import bodyParser from 'body-parser'
 import compression from 'compression'
 import logger from 'morgan'
 import { desencrypt } from './src/controllers/encrypt'
-import { getVentas } from './src/controllers/consultas'
+import { getBalance, getResultados, getVentas } from './src/controllers/consultas'
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -52,14 +52,39 @@ app.get('/ventas', validateToken, async (req, res) => {
     const [identificacion, user, password] = split
 
     console.log('decrypted', decrypted)
-    //const resultado = await getVentas(user, password)
-    res.status(200).json(decrypted)
+    const resultado = await getVentas(user, password)
+    res.status(200).json(resultado)
   } catch (error) {
     console.error('Error en /: ', error)
     res.status(500).json({ message: 'Internal server error' })
   }
 })
-
+app.get(`/resultados`, validateToken, async (req, res) => {
+  try {
+    const clave = req.headers.clave
+    const decrypted = await desencrypt(clave)
+    const split = decrypted.split('%')
+    const [identificacion, user, password] = split
+    const resultado = await getResultados(user, password)
+    res.status(200).json(resultado)
+  } catch (error) {
+    console.error('Error en /: ', error)
+    res.status(500).json({ message: 'Internal server error' })
+  }
+})
+app.get(`/balance`, validateToken, async (req, res) => {
+  try {
+    const clave = req.headers.clave
+    const decrypted = await desencrypt(clave)
+    const split = decrypted.split('%')
+    const [identificacion, user, password] = split
+    const resultado = await getBalance(user, password)
+    res.status(200).json(resultado)
+  } catch (error) {
+    console.error('Error en /: ', error)
+    res.status(500).json({ message: 'Internal server error' })
+  }
+})
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
